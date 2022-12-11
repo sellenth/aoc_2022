56e7b0c9e881dc6122c1a75d2dff48bfb10db436
halston <sellenth@oregonstate.edu>
2022-12-11 19:57:30 +0700

    finish day9

---
+++ day10/day10_part1.ts
@@ -0,0 +1,15 @@
+import { readFileSync, promises as fsPromises } from 'fs';
+import { join } from 'path';
+
+
+let test_input = ``
+
+function syncReadFile(filename: string) {
+  let result = readFileSync(join(__dirname, filename), 'utf-8');
+  return result;
+}
+
+let real_input = syncReadFile('./input.txt');
+
+let testing = false
+let input = testing ? test_input : real_input;
