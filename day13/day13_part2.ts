c7bf3449d5b5298e608b5718666e0c2bc326e5b7
halston <sellenth@oregonstate.edu>
2022-12-15 10:36:03 +0700

    finish day13. woo

--- day13/day13_part2.ts
+++ day13/day13_part2.ts
@@ -1,16 +1,29 @@
 import { readFileSync, promises as fsPromises } from 'fs';
 import { join } from 'path';
 
-
-//let test_input = `[9]
-//[[8,7,6]]
-//`
-
-/*
- *
- */
 let test_input = `[1,1,3,1,1]
-[1,1,3,1,1]`
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`
 
 function syncReadFile(filename: string) {
   let result = readFileSync(join(__dirname, filename), 'utf-8');
@@ -31,23 +44,23 @@ function parse_input(input: string) {
   let output: any = []
   let pairs_raw = input.split('\n\n')
   for (let i = 0; i < pairs_raw.length; ++i) {
-  //for (let i = 6; i < 7; ++i) {
     let [left, right] = pairs_raw[i].split('\n')
-    output.push( [parse_line(left), parse_line(right) ] )
+    output.push( parse_line(left), parse_line(right) )
   }
   return output
 }
 
-let pairs = parse_input(input)
+let packets = parse_input(input)
+packets.push([[2]], [[6]])
 
-type troolean = true | false | "neither"
+type troolean = 1 | -1 | 0
 
 function compare(left: any, right: any): troolean {
   let right_idx = 0
   for (let left_idx = 0; left_idx < left.length; ++left_idx, ++right_idx) {
     if (right_idx >= right.length) {
       console.log('right ran out of items')
-      return false
+      return -1
     }
 
     let l_is_arr = left[left_idx] instanceof Array
@@ -55,51 +68,51 @@ function compare(left: any, right: any): troolean {
 
     if (l_is_arr && r_is_arr) {
       let res = compare(left[left_idx], right[right_idx])
-      if (res !== 'neither') {
+      if (res !== 0) {
         return res
       }
     }
     else if (!l_is_arr && r_is_arr) {
       let res = compare([left[left_idx]], right[right_idx])
-      if (res !== 'neither') {
+      if (res !== 0) {
         return res
       }
     }
     else if (l_is_arr && !r_is_arr) {
       let res = compare(left[left_idx], [right[right_idx]])
-      if (res !== 'neither') {
+      if (res !== 0) {
         return res
       }
     }
     else if (!l_is_arr && !r_is_arr) {
       if (left[left_idx] < right[right_idx]) {
-        return true
+        return 1
       } else if (left[left_idx] > right[right_idx]) {
-        console.log(left[left_idx], right[right_idx])
         console.log('right is less than left')
-        return false
+        return -1
       }
     }
   }
 
   if (right_idx < right.length) {
-    return true
+    return 1
   }
 
-  return "neither"
-}
-
-let pairs_in_order: number[] = []
-
-for (let i = 0; i < pairs.length; ++i)
-{
-  let [left, right] = pairs[i]
-  if ([true, "neither"].includes(compare(left, right)) ) {
-  //if (compare(left, right) == true ) {
-    pairs_in_order.push(i + 1)
-  }
+  return 0
 }
 
 
-console.log(pairs_in_order)
-console.log(pairs_in_order.reduce( (sum, curr) => sum + curr, 0 ))
+let sorted: any[] = packets.sort( (a: any, b: any) => {
+  return compare(a, b)
+} ).reverse()
+
+let decoder_key = 1
+
+for (let i = 0; i < sorted.length; ++i) {
+  let item = JSON.stringify(sorted[i])
+
+  if (item == '[[2]]' || item == '[[6]]')
+    decoder_key *= (i + 1)
+}
+
+console.log(decoder_key)
